6db96f9dbb67cae08c51acf309873adaef4944cc
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-09-20 22:14:29 +0800

    fix a body overflow bug

--- src/index.js
+++ src/index.js
@@ -223,7 +223,7 @@ export default {
 
       this.onClose && this.onClose();
 
-      if (this.modal) {
+      if (this.modal && this.bodyOverflow !== 'hidden') {
         document.body.style.overflow = this.bodyOverflow;
       }
       this.opened = false;
